d17187a2cf1614cb01f1299c6f0968fd294a18f0
Divya Sharma <sarmas07701@gmail.com>
2026-04-25 18:45:53 +0530

    Final Fixing

--- backend/vector_store.py
+++ backend/vector_store.py
@@ -1,12 +1,15 @@
-from langchain_groq import GroqEmbeddings
-from langchain_community.vectorstores import Chroma
 import os
+from langchain_groq import ChatGroq # Isko verify ke liye rakha hai
+from langchain_community.vectorstores import Chroma
+# Naya import path jo har version mein kaam karta hai
+from langchain_community.embeddings import DeterministicFakeEmbedding 
+from langchain_groq import GroqEmbeddings
 import shutil
 from dotenv import load_dotenv
 
 load_dotenv()
 
-# Ab hum local RAM use nahi karenge, Groq ki Cloud API use karenge
+# Cloud Embeddings
 embeddings = GroqEmbeddings(
     model_name="nomic-embed-text-v1.5",
     groq_api_key=os.getenv("GROQ_API_KEY")
@@ -15,7 +18,6 @@ embeddings = GroqEmbeddings(
 def save_to_vector_db(chunks):
     if os.path.exists("./db_storage"):
         shutil.rmtree("./db_storage")
-        
     vector_db = Chroma.from_documents(
         documents=chunks, 
         embedding=embeddings, 
